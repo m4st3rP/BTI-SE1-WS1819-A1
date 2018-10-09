2b9ce0d019eec57042daae355fdf1d6209a18339
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-10-09 19:46:32 +0200

    added comments

--- wrapper/src/wrapper/SensorAPI.java
+++ wrapper/src/wrapper/SensorAPI.java
@@ -3,11 +3,12 @@ package wrapper;
 public class SensorAPI {
     public int calculateCentimetersFromDiscreteValue(char value) {
         var value_d = (double) value;
-
+        
+        // revert the formula
         value_d += 5.0;
         value_d /= 265.0;
         value_d = 1.0 - value;
-        value_d = Math.acos(value_d);
+        value_d = Math.acos(value_d); // acos gives us back the angle we put into cos
         value_d = value_d / (Math.PI/2);
         value_d *= 30;
 
